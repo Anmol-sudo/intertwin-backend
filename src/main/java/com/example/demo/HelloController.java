ae6c44368cf32b7b3894491a54a1e5c1d269e051
Anmol-sudo <anmolshiv1974@gmail.com>
2026-02-28 10:14:23 +0530

    Path Variables (dynamic URLs) with the route "/hello/{name}"

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+// Control all routes
 @RestController
 public class HelloController {
-
+    // A GET Route
     @GetMapping("/")
     public String hello() {
         return "Hello Spring Boot 🚀";
+    }
+
+    // Path Variables Dynamic URLs
+    @GetMapping("/hello/{name}")
+    public String helloUser(@PathVariable String name) {
+        return "Hello " + name + " 👋";
     }
 }
